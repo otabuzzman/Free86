a8fd8415f7bcca859a17146af3c66d0343a5b54b
otabuzzman <iuergen.schuck@gmail.com>
2025-12-12 11:29:12 +0100

    Use memory access functions

--- src/x86/fedex.cpp
+++ src/x86/fedex.cpp
@@ -939,7 +939,7 @@ void Free86::fetch_decode_execute(uint64_t cycles) {
             case 0x5f: // POP DI
                 if (x86_64_long_mode) {
                     mem8_loc = regs[4];
-                    ld32_readonly_cpl3();
+                    x = ld32_readonly_cpl3();
                     regs[4] = mem8_loc + 4;
                 } else {
                     x = read_stack_dword();
