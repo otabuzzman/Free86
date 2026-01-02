933d32644d5c17ec5be40798fa61924078aa4fda
otabuzzman <iuergen.schuck@gmail.com>
2026-01-02 23:44:42 +0100

    Save work on Swift port

--- Tests/suite/GeneralRegister.swift
+++ Tests/suite/GeneralRegister.swift
@@ -8,13 +8,16 @@ func generalRegisterElements() {
     #expect(register.byteL == 0xFE)
     #expect(register.byteH == 0xCA)
     #expect(register.wordX == 0xCAFE)
+
     register.wordX = 0xBEEF
     #expect(register.byteL == 0xEF)
     #expect(register.byteH == 0xBE)
     #expect(register.wordX == 0xBEEF)
+
     register.byteL = 0x55
     register.byteH = 0xAA
     #expect(register == 0xDEADAA55)
+
     register.upperHalf = 0xC0DE
     #expect(register == 0xC0DEAA55)
 }
