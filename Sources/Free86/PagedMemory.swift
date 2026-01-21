024a36243023109576dd958b86ff912a58613266
otabuzzman <iuergen.schuck@gmail.com>
2026-01-21 10:01:12 +0100

    Save work on TLB and paging

--- Sources/Free86/PagedMemory.swift
+++ Sources/Free86/PagedMemory.swift
@@ -1,21 +1,21 @@
 protocol PagedMemory {
-    /// load byte/ word/ dword as supervisor from read-only memory
+    /// load byte/ word/ dword from read-only supervisor memory
     func ld8FromReadonly()
     func ld16FromReadonly()
     func ldFromReadonly()
-    /// load byte/ word/ dword as user from read-only memory
+    /// load byte/ word/ dword from read-only user memory
     func ld8FromUserReadonly()
     func ld16FromUserReadonly()
     func ldFromUserReadonly()
-    /// load byte/ word/ dword as user from writable memory
+    /// load byte/ word/ dword from writable user memory
     func ld8FromUserWritable()
     func ld16FromUserWritable()
     func ldFromUserWritable()
-    /// store byte/ word/ dword as supervisor in read-only memory
+    /// store byte/ word/ dword in read-only supervisor memory
     func st8InReadonly(byte: Byte)
     func st16InReadonly(word: Word)
     func stInReadonly(dword: DWord)
-    /// store byte/ word/ dword as user in read-only memory
+    /// store byte/ word/ dword in read-only user memory
     func st8InUserReadonly(byte: Byte)
     func st16InUserReadonly(word: Word)
     func stInUserReadonly(dword: DWord)
