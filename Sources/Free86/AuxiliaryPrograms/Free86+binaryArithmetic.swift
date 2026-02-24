30413b10259f67db781a3fb28cee616b82676b5b
otabuzzman <iuergen.schuck@gmail.com>
2026-02-24 14:17:45 +0100

    Save work on binary arithmetic auxiliary programs

--- Sources/Free86/AuxiliaryPrograms/Free86+binaryArithmetic.swift
+++ Sources/Free86/AuxiliaryPrograms/Free86+binaryArithmetic.swift
@@ -147,29 +147,251 @@ extension Free86 {
         }
     }
     func aux8Mul(_ multiplicand: DWord, _ multiplier: DWord) {
+        u = (multiplicand & 0xff) &* (multiplier & 0xff)
+        osmSrc = u >> 8
+        osmDst = u.signExtendedByte
+        osm = 21
     }
     func aux16Mul(_ multiplicand: DWord, _ multiplier: DWord) {
+        u = (multiplicand & 0xffff) &* (multiplier & 0xffff)
+        osmSrc = u >> 16
+        osmDst = u.signExtendedWord
+        osm = 22
     }
     func auxMul(_ multiplicand: DWord, _ multiplier: DWord) {
+        multiply(multiplicand, multiplier)
+        osmDst = u
+        osmSrc = v
+        osm = 23
     }
     func aux8Imul(_ multiplicand: DWord, _ multiplier: DWord) {
+        let md = multiplicand.signExtendedByte
+        let mr = multiplier.signExtendedByte
+        u = md &* mr
+        osmDst = u.signExtendedByte
+        osmSrc = (u != osmDst) ? 1 : 0
+        osm = 21
     }
     func aux16Imul(_ multiplicand: DWord, _ multiplier: DWord) {
+        let md = multiplicand.signExtendedWord
+        let mr = multiplier.signExtendedWord
+        u = md &* mr
+        osmDst = u.signExtendedWord
+        osmSrc = (u != osmDst) ? 1 : 0
+        osm = 22
     }
     func auxImul(_ multiplicand: DWord, _ multiplier: DWord) {
+        let md = multiplicand
+        let mr = multiplier
+        var s: DWord = 0
+        if (md & 0x80000000) != 0 {
+            md = ~md &+ 1
+            s = 1
+        }
+        if (mr & 0x80000000) != 0 {
+            mr = ~mr &+ 1
+            s ^= 1
+        }
+        multiply(md, mr)
+        if s != 0 {
+            v = ~v
+            u = ~u &+ 1
+            if u == 0 {
+                v = v &+ 1
+            }
+    }
+    osmDst = u
+    osmSrc = v - u.signedShiftRight(31)
+    osm = 23
     }
     func multiply(_ multiplicand: DWord, _ multiplier: DWord) {
+        var x = QWord(multiplicand) &* QWord(multiplier)
+        if x <= 0xffffffff {
+            v = 0
+        } else {
+            let dl = multiplicand & 0xffff
+            let du = multiplicand >> 16
+            let rl = multiplier & 0xffff
+            let ru = multiplier >> 16
+            x = dl &* rl
+            v = du &* ru
+            w = dl &* ru
+            x = x &+ (w & 0xffff) << 16
+            v = v &+ (w >> 16)
+            if x >= 4294967296 {
+                x -= 4294967296
+                v = v &+ 1
+            }
+            w = du &* rl
+            x = x &+ (w & 0xffff) << 16
+            v = v &+ (w >> 16)
+            if x >= 4294967296 {
+                x -= 4294967296
+                v = v &+ 1
+            }
+        }
+        u = DWord(truncatingIfNeeded: x)
     }
     @discardableResult
     func calculate8(_ dst: DWord, _ src: DWord) -> DWord {
-        0
+        let cf: DWord, res: DWord
+        res = dst
+        switch operation & 7 {
+        case 0:
+            osmSrc = src
+            res = (res &+ src).signExtendedByte
+            osmDst = res
+            osm = 0
+            break
+        case 1:
+            res = (res | src).signExtendedByte
+            osmDst = res
+            osm = 12
+            break
+        case 2:
+            cf = isCF() ? 1 : 0
+            osmSrc = src
+            res = (res &+ src &+ cf).signExtendedByte
+            osmDst = res
+            osm = cf != 0 ? 3 : 0
+            break
+        case 3:
+            cf = isCF() ? 1 : 0
+            osmSrc = src
+            res = (res &- src &- cf).signExtendedByte
+            osmDst = res
+            osm = cf != 0 ? 9 : 6
+            break
+        case 4:
+            res = (res & src).signExtendedByte
+            osmDst = res
+            osm = 12
+            break
+        case 5:
+            osmSrc = src
+            res = (res &- src).signExtendedByte
+            osmDst = res
+            osm = 6
+            break
+        case 6:
+            res = (res ^ src).signExtendedByte
+            osmDst = res
+            osm = 12
+            break
+        case 7:
+            osmSrc = src
+            osmDst = (dst &- src).signExtendedByte
+            osm = 6
+            break
+        }
+        return res
     }
     @discardableResult
     func calculate16(_ dst: DWord, _ src: DWord) -> DWord {
-        0
+        let cf: DWord, res: DWord
+        res = dst
+        switch operation & 7 {
+        case 0:
+            osmSrc = src
+            res = (res &+ src).signExtendedWord
+            osmDst = res
+            osm = 1
+            break
+        case 1:
+            res = (res | src).signExtendedWord
+            osmDst = res
+            osm = 13
+            break
+        case 2:
+            cf = isCF() ? 1 : 0
+            osmSrc = src
+            res = (res &+ src &+ cf).signExtendedWord
+            osmDst = res
+            osm = cf != 0 ? 4 : 1
+            break
+        case 3:
+            cf = isCF() ? 1 : 0
+            osmSrc = src
+            res = (res &- src &- cf).signExtendedWord
+            osmDst = res
+            osm = cf != 0 ? 10 : 7
+            break
+        case 4:
+            res = (res & src).signExtendedWord
+            osmDst = res
+            osm = 13
+            break
+        case 5:
+            osmSrc = src
+            res = (res &- src).signExtendedWord
+            osmDst = res
+            osm = 7
+            break
+        case 6:
+            res = (res ^ src).signExtendedWord
+            osmDst = res
+            osm = 13
+            break
+        case 7:
+            osmSrc = src
+            osmDst = (res &- src).signExtendedWord
+            osm = 7
+            break
+        }
+        return res
     }
     @discardableResult
     func calculate(_ dst: DWord, _ src: DWord) -> DWord {
-        0
+        let cf: DWord, res: DWord
+        res = dst
+        switch operation & 7 {
+        case 0:
+            osmSrc = src
+            res = res &+ src
+            osmDst = res
+            osm = 2
+            break
+        case 1:
+            res = res | src
+            osmDst = res
+            osm = 14
+            break
+        case 2:
+            cf = isCF() ? 1 : 0
+            osmSrc = src
+            res = res &+ src &+ cf
+            osmDst = res
+            osm = cf != 0 ? 5 : 2
+            break
+        case 3:
+            cf = isCF() ? 1 : 0
+            osmSrc = src
+            res = res &- src &- cf
+            osmDst = res
+            osm = cf != 0 ? 11 : 8
+            break
+        case 4:
+            res = res & src
+            osmDst = res
+            osm = 14
+            break
+        case 5:
+            osmSrc = src
+            res = res &- src
+            osmDst = res
+            osm = 8
+            break
+        case 6:
+            res = res ^ src
+            osmDst = res
+            osm = 14
+            break
+        case 7:
+            osmSrc = src
+            osmDst = res &- src
+            osm = 8
+            break
+        }
+        return res
     }
 }
