469815f386d08a3ce82fe57487a95e6a41073500
otabuzzman <iuergen.schuck@gmail.com>
2026-03-29 19:55:34 +0200

    Remove opcode bit-fiddling

--- Tests/suite/Opcode.swift
+++ Tests/suite/Opcode.swift
@@ -18,3 +18,22 @@ func opcodeFieldsAccess() {
     #expect(opcode.isOdd == false)
     #expect(opcode.isEven == true)
 }
+
+@Test("opcode encoded fields access")
+func opcodeEncodedFieldsAccess() {
+    var opcode: Opcode = 0xA5  // 0b1010_0101
+    #expect(opcode.encoded(.operation) == 4)
+    #expect(opcode.encoded(.condition) == 5)
+    #expect(opcode.encoded(.generalRegister) == 5)
+    #expect(opcode.encoded(.segmentRegister) == 5)
+    #expect(opcode.encoded(.standardSegmentRegister) == 4)
+    #expect(opcode.encoded(.extendedSegmentRegister) == 4)
+
+    opcode = 0x5A  // 0b0101_1010
+    #expect(opcode.encoded(.operation) == 3)
+    #expect(opcode.encoded(.condition) == 10)
+    #expect(opcode.encoded(.generalRegister) == 2)
+    #expect(opcode.encoded(.segmentRegister) == 2)
+    #expect(opcode.encoded(.standardSegmentRegister) == 3)
+    #expect(opcode.encoded(.extendedSegmentRegister) == 3)
+}
