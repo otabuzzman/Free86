cc2318f30c88aac5da8532daa91ebe944c4e6437
otabuzzman <iuergen.schuck@gmail.com>
2026-08-26 20:24:33 +0200

    Fix and simplify string code

--- Sources/Free86/AuxiliaryPrograms/Free86+stringManipulation.swift
+++ Sources/Free86/AuxiliaryPrograms/Free86+stringManipulation.swift
@@ -220,7 +220,7 @@ extension Free86 {
             }
             u = io?[edx] ?? 0
             lax = segs[.ES].shadow.base &+ (edi & mask)
-            try st8WritableCpl3(byte: u)
+            try st16WritableCpl3(word: u)
             regs[.EDI] = (edi & ~mask) | ((edi &+ df << 1) & mask)
             regs[.ECX] = (ecx & ~mask) | ((ecx &- 1) & mask)
             ecx = regs[.ECX]
@@ -230,7 +230,7 @@ extension Free86 {
         } else {
             u = io?[edx] ?? 0
             lax = segs[.ES].shadow.base &+ (edi & mask)
-            try st8WritableCpl3(byte: u)
+            try st16WritableCpl3(word: u)
             regs[.EDI] = (edi & ~mask) | ((edi &+ df << 1) & mask)
         }
     }
@@ -367,7 +367,7 @@ extension Free86 {
                 return
             }
             u = DWord(try ld16ReadonlyCpl3())
-            regs[.EAX] = u
+            regs[.EAX] = (regs[.EAX] & 0xffff0000) | u
             regs[.ESI] = (esi & ~mask) | ((esi &+ df << 1) & mask)
             regs[.ECX] = (ecx & ~mask) | ((ecx &- 1) & mask)
             ecx = regs[.ECX]
@@ -376,218 +376,11 @@ extension Free86 {
             }
         } else {
             u = DWord(try ld16ReadonlyCpl3())
-            regs[.EAX] = u
+            regs[.EAX] = (regs[.EAX] & 0xffff0000) | u
             regs[.ESI] = (esi & ~mask) | ((esi &+ df << 1) & mask)
         }
     }
     func auxScasw() throws {
-        let mask = ipr.addressSizeMask
-        let edi = regs[.EDI]
-        lax = segs[.ES].shadow.base &+ (edi & mask)
-        operation = 7
-        if ipr.isFlagRaised(.repzStringOperation) || ipr.isFlagRaised(.repnzStringOperation) {
-            var ecx = regs[.ECX]
-            if ecx & mask == 0 {
-                return
-            }
-            u = DWord(try ld16ReadonlyCpl3())
-            calculate16(regs[.EAX], u)
-            regs[.EDI] = (edi & ~mask) | ((edi &+ df << 1) & mask)
-            regs[.ECX] = (ecx & ~mask) | ((ecx &- 1) & mask)
-            ecx = regs[.ECX]
-            if ipr.isFlagRaised(.repzStringOperation) {
-                if !(osmDst == 0) {
-                    return
-                }
-            } else {
-                if osmDst == 0 {
-                    return
-                }
-            }
-            if ecx & mask != 0 {
-                far = farStart
-            }
-        } else {
-            u = DWord(try ld16ReadonlyCpl3())
-            calculate16(regs[.EAX], u)
-            regs[.EDI] = (edi & ~mask) | ((edi &+ df << 1) & mask)
-        }
-    }
-    func aux16Ins() throws {
-        if cpl > eflags.iopl {
-            throw Interrupt(.GP, errorCode: 0)
-        }
-        let mask = ipr.addressSizeMask
-        let edi = regs[.EDI]
-        let edx = regs[.EDX] & 0xffff
-        if ipr.isFlagRaised(.repzStringOperation) || ipr.isFlagRaised(.repnzStringOperation) {
-            var ecx = regs[.ECX]
-            if ecx & mask == 0 {
-                return
-            }
-            u = io?[edx] ?? 0
-            lax = segs[.ES].shadow.base &+ (edi & mask)
-            try st8WritableCpl3(byte: u)
-            regs[.EDI] = (edi & ~mask) | ((edi &+ df << 1) & mask)
-            regs[.ECX] = (ecx & ~mask) | ((ecx &- 1) & mask)
-            ecx = regs[.ECX]
-            if ecx & mask != 0 {
-                far = farStart
-            }
-        } else {
-            u = io?[edx] ?? 0
-            lax = segs[.ES].shadow.base &+ (edi & mask)
-            try st8WritableCpl3(byte: u)
-            regs[.EDI] = (edi & ~mask) | ((edi &+ df << 1) & mask)
-        }
-    }
-    func aux16Outs() throws {
-        if cpl > eflags.iopl {
-            throw Interrupt(.GP, errorCode: 0)
-        }
-        let mask = ipr.addressSizeMask
-        let sreg = ipr.segmentRegister
-        let esi = regs[.ESI]
-        let edx = regs[.EDX] & 0xffff
-        if ipr.isFlagRaised(.repzStringOperation) || ipr.isFlagRaised(.repnzStringOperation) {
-            var ecx = regs[.ECX]
-            if ecx & mask == 0 {
-                return
-            }
-            lax = segs[sreg].shadow.base &+ (esi & mask)
-            u = DWord(try ld16ReadonlyCpl3())
-            io?[edx] = u
-            regs[.ESI] = (esi & ~mask) | ((esi &+ df << 1) & mask)
-            regs[.ECX] = (ecx & ~mask) | ((ecx &- 1) & mask)
-            ecx = regs[.ECX]
-            if ecx & mask != 0 {
-                far = farStart
-            }
-        } else {
-            lax = segs[sreg].shadow.base &+ (esi & mask)
-            u = DWord(try ld16ReadonlyCpl3())
-            io?[edx] = u
-            regs[.ESI] = (esi & ~mask) | ((esi &+ df << 1) & mask)
-        }
-    }
-    func aux16Movs() throws {
-        let mask = ipr.addressSizeMask
-        let sreg = ipr.segmentRegister
-        let esi = regs[.ESI]
-        let edi = regs[.EDI]
-        lax = segs[sreg].shadow.base &+ (esi & mask)
-        let la = segs[.ES].shadow.base &+ (edi & mask)
-        if ipr.isFlagRaised(.repzStringOperation) || ipr.isFlagRaised(.repnzStringOperation) {
-            var ecx = regs[.ECX]
-            if ecx & mask == 0 {
-                return
-            }
-            u = DWord(try ld16ReadonlyCpl3())
-            lax = la
-            try st16WritableCpl3(word: u)
-            regs[.ESI] = (esi & ~mask) | ((esi &+ df << 1) & mask)
-            regs[.EDI] = (edi & ~mask) | ((edi &+ df << 1) & mask)
-            regs[.ECX] = (ecx & ~mask) | ((ecx &- 1) & mask)
-            ecx = regs[.ECX]
-            if ecx & mask != 0 {
-                far = farStart
-            }
-        } else {
-            u = DWord(try ld16ReadonlyCpl3())
-            lax = la
-            try st16WritableCpl3(word: u)
-            regs[.ESI] = (esi & ~mask) | ((esi &+ df << 1) & mask)
-            regs[.EDI] = (edi & ~mask) | ((edi &+ df << 1) & mask)
-        }
-    }
-    func aux16Stos() throws {
-        let mask = ipr.addressSizeMask
-        let edi = regs[.EDI]
-        lax = segs[.ES].shadow.base &+ (edi & mask)
-        if ipr.isFlagRaised(.repzStringOperation) || ipr.isFlagRaised(.repnzStringOperation) {
-            var ecx = regs[.ECX]
-            if ecx & mask == 0 {
-                return
-            }
-            try st16WritableCpl3(word: regs[.EAX])
-            regs[.EDI] = (edi & ~mask) | ((edi &+ df << 1) & mask)
-            regs[.ECX] = (ecx & ~mask) | ((ecx &- 1) & mask)
-            ecx = regs[.ECX]
-            if ecx & mask != 0 {
-                far = farStart
-            }
-        } else {
-            try st16WritableCpl3(word: regs[.EAX])
-            regs[.EDI] = (edi & ~mask) | ((edi &+ df << 1) & mask)
-        }
-    }
-    func aux16Cmps() throws {
-        let mask = ipr.addressSizeMask
-        let sreg = ipr.segmentRegister
-        let esi = regs[.ESI]
-        let edi = regs[.EDI]
-        lax = segs[sreg].shadow.base &+ (esi & mask)
-        let la = segs[.ES].shadow.base &+ (edi & mask)
-        operation = 7
-        if ipr.isFlagRaised(.repzStringOperation) || ipr.isFlagRaised(.repnzStringOperation) {
-            var ecx = regs[.ECX]
-            if ecx & mask == 0 {
-                return
-            }
-            u = DWord(try ld16ReadonlyCpl3())
-            lax = la
-            v = DWord(try ld16ReadonlyCpl3())
-            calculate16(u, v)
-            regs[.ESI] = (esi & ~mask) | ((esi &+ df << 1) & mask)
-            regs[.EDI] = (edi & ~mask) | ((edi &+ df << 1) & mask)
-            regs[.ECX] = (ecx & ~mask) | ((ecx &- 1) & mask)
-            ecx = regs[.ECX]
-            if ipr.isFlagRaised(.repzStringOperation) {
-                if !(osmDst == 0) {
-                    return
-                }
-            } else {
-                if osmDst == 0 {
-                    return
-                }
-            }
-            if ecx & mask != 0 {
-                far = farStart
-            }
-        } else {
-            u = DWord(try ld16ReadonlyCpl3())
-            lax = la
-            v = DWord(try ld16ReadonlyCpl3())
-            calculate16(u, v)
-            regs[.ESI] = (esi & ~mask) | ((esi &+ df << 1) & mask)
-            regs[.EDI] = (edi & ~mask) | ((edi &+ df << 1) & mask)
-        }
-    }
-    func aux16Lods() throws {
-        let mask = ipr.addressSizeMask
-        let sreg = ipr.segmentRegister
-        let esi = regs[.ESI]
-        lax = segs[sreg].shadow.base &+ (esi & mask)
-        if ipr.isFlagRaised(.repzStringOperation) || ipr.isFlagRaised(.repnzStringOperation) {
-            var ecx = regs[.ECX]
-            if ecx & mask == 0 {
-                return
-            }
-            u = DWord(try ld16ReadonlyCpl3())
-            regs[.EAX] = (regs[.EAX] & 0xffff0000) | u
-            regs[.ESI] = (esi & ~mask) | ((esi &+ df << 1) & mask)
-            regs[.ECX] = (ecx & ~mask) | ((ecx &- 1) & mask)
-            ecx = regs[.ECX]
-            if ecx & mask != 0 {
-                far = farStart
-            }
-        } else {
-            u = DWord(try ld16ReadonlyCpl3())
-            regs[.EAX] = (regs[.EAX] & 0xffff0000) | u
-            regs[.ESI] = (esi & ~mask) | ((esi &+ df << 1) & mask)
-        }
-    }
-    func aux16Scas() throws {
         let mask = ipr.addressSizeMask
         let edi = regs[.EDI]
         lax = segs[.ES].shadow.base &+ (edi & mask)
